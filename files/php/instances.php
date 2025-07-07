4396d35faedc28ea0ea1af73a76f3bb5a93bb74d
SROff23712 <sroff2371@gmail.com>
2025-07-07 23:30:37 +0200

    Add files via upload

---
+++ files/php/instances.php
@@ -0,0 +1,18 @@
+<?php
+$instance['SurvieGolmon'] = array_merge($instance['SurvieGolmon'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.21.5",
+        "loadder_type" => "NeoForge",
+        "loadder_version" => "latest"
+    ),
+    "verify" => false,
+    "ignored" => array(),
+    "whitelist" => array(),
+    "whitelistActive" => false,
+    "status" => array(
+        "nameServer" => "SurvieGolmon",
+        "ip" => "survieteamgolmon.falixsrv.me",
+        "port" => 22025
+    )
+));
+?>
